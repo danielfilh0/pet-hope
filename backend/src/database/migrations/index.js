f395fb71da84641f3ca12452847636285364bd56
Daniel Filho <danbsilva98@gmail.com>
2023-10-16 16:05:00 -0300

    change DB tables adding alter table

--- backend/src/database/migrations/index.js
+++ backend/src/database/migrations/index.js
@@ -26,6 +26,12 @@ query(`
     status VARCHAR(20) CHECK (status IN ('PENDING', 'ACCEPTED')),
     FOREIGN KEY (pet_id) REFERENCES Pets(id)
   );
+
+  ALTER TABLE users ALTER COLUMN name SET NOT NULL, ALTER COLUMN email SET NOT NULL, ALTER COLUMN password SET NOT NULL;
+  ALTER TABLE pets ALTER COLUMN name SET NOT NULL, ALTER COLUMN type SET NOT NULL, ALTER COLUMN age SET NOT NULL, ALTER COLUMN was_adopted SET NOT NULL;
+  ALTER TABLE adoption_requests ALTER COLUMN pet_id SET NOT NULL, ALTER COLUMN contact_name SET NOT NULL, ALTER COLUMN contact_phone SET NOT NULL, ALTER COLUMN contact_address SET NOT NULL, ALTER COLUMN status SET NOT NULL;
+
+  ALTER TABLE pets ADD COLUMN description TEXT;
 `).then(() => {
   console.log('Database created successfully!')
 }).catch(() => {
